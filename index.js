2b05f0821bec92ab09c76663c4ad216c1609780a
Samuel Barbosa da Silva <103833450+osamueldasilva@users.noreply.github.com>
2023-03-29 20:24:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const divUsuario = document.getElementById("usuario");
+
+// Exibe o prompt para o usuário
+const nome = window.prompt("Qual seu nome?");
+
+// Adiciona o valor da resposta na div
+divUsuario.innerText = `Olá ${nome}, é um prazer receber você em meu portfólio, espero que goste.`;
+
+
+
+
+
+
 
 function scrollToSection2() {
   document.querySelector('#section_2').scrollIntoView({
